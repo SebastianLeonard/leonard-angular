5842653ef54f98d73481cb8224baac98c97f2f36
Sebastian Leonard <saleonard@my.waketech.edu>
2023-07-17 22:25:01 -0400

    Angular Task Tracker App v1.0.0
    proper code for Task Tracking App provided by the assigned YouTube video.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'leonard-angular';
-}
+export class AppComponent {}
